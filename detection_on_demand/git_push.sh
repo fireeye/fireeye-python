2fd7b87dadb0807f76c3c62a6673001b0ebaee76
Colin McKibben <colin.mckibben@fireeye.com>
2020-01-24 10:54:36 -0500

    Fixed DoD git location

--- detection_on_demand/git_push.sh
+++ detection_on_demand/git_push.sh
@@ -19,7 +19,7 @@ if [ "$git_user_id" = "" ]; then
 fi
 
 if [ "$git_repo_id" = "" ]; then
-    git_repo_id="fireeye-python/tree/test/openapi-generator"
+    git_repo_id="fireeye-python/tree/test/openapi-generator/detection_on_demand"
     echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
 fi
 
